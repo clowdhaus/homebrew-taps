d7762cbec1d921075ce11214f27caf2f5fa7983d
Bryant Biggs <bryantbiggs@gmail.com>
2024-07-05 17:41:36 -0400

    feat: Update `cookiecluster` to `v0.2.1`

--- Formula/cookiecluster.rb
+++ Formula/cookiecluster.rb
@@ -1,27 +1,27 @@
 class Cookiecluster < Formula
   desc "Stamping out clusters like its 1999"
   homepage "https://github.com/clowdhaus/cookiecluster"
-  version "v0.2.0"
+  version "v0.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "6b327904e6066a0c6bb919081a05cac33a3520aae2dbce0840f86f736fb493f9"
+      sha256 "0341f61fa6cb8fedba0902ede56ab371983003060bbeb57a61834041b8f15829"
     end
     if Hardware::CPU.intel?
       url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256  "ccfa8bb2b5de42926494373e6e386d854c709649307a26af1819b208861f310f"
+      sha256  "1e2b688376980f5d5f0e449d6b41e06d761d6d1910883766aa81907d19576da2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-arm-unknown-linux-gnueabihf.tar.gz"
-      sha256 "64bc1be31b8e8e33adf860729db4c905033646536f763a244d83d22d10f5be96"
+      sha256 "f311bb0d77434bc3207baa2219a96add1ddcf88ef913f35af5ffb6dd6ad99627"
     end
     if Hardware::CPU.intel?
       url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "9755e3ca476047c1247c5f67c64c91b59c1af08d1bd9e878edf1b1642989d7b9"
+      sha256 "1ef197303583995a2baa3542e52ecbe2a2c081742ee686cb13a0e61ca74da5b8"
     end
   end
 
