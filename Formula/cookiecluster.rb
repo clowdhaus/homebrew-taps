b31f318961d9d2ac0ea4461c7b4ca77785b0d234
Bryant Biggs <bryantbiggs@gmail.com>
2025-12-05 08:37:43 -0600

    feat: Update `cookiecluster` to `v0.13.0`

--- Formula/cookiecluster.rb
+++ Formula/cookiecluster.rb
@@ -1,28 +1,26 @@
 class Cookiecluster < Formula
   desc "Stamping out clusters like its 1999"
   homepage "https://github.com/clowdhaus/cookiecluster"
-  version "v0.10.0"
+  version "v0.13.0"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "958ea9a6a3ff85e5f100de906f0fc684bb38b00beb2a8e58b293609ec94d1c51"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256  "ce40f8b7131009910877208f6ede559821926cdd84e80830e856ad4fe3f51350"
-    end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-aarch64-apple-darwin.tar.gz"
+    sha256 "b91cd30df27022f3603f5d26c133e478e40cffcd5158dea5cb460306b379445f"
   end
 
-  on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-arm-unknown-linux-gnueabihf.tar.gz"
-      sha256 "06f9d9279aa947ffe45d738a682dd364b9f188a7dda52159b290d8cae31a40ca"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "1812cca1daae89b6d18b9e68a79cacbfe66c11107587b1bf9b0cba919b9e0d0d"
-    end
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-x86_64-apple-darwin.tar.gz"
+    sha256  "8ac30d5045498ea6bc1e9331bbc7f1241b72f2a3d20b88e7e04c7d2a5479e7a7"
+  end
+
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "3dcaf80932ed48dd0af50253fdde4b05d21f964c9e8e7836bb9e94620a68d1b5"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+      url "https://github.com/clowdhaus/cookiecluster/releases/download/#{version}/cookiecluster-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "cf5f5ca5093a6fb2e4f5d9f594a0d5f6dd8e1c46f364bf484c7302404a460876"
   end
 
   def install
