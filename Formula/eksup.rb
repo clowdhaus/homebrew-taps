907477d61d985d5dc833a95442d87d08f6313758
Bryant Biggs <bryantbiggs@gmail.com>
2026-02-20 16:56:53 -0600

    feat: Update `eksup` to `v0.13.0`

--- Formula/eksup.rb
+++ Formula/eksup.rb
@@ -1,26 +1,26 @@
 class Eksup < Formula
   desc "A CLI to aid in upgrading Amazon EKS clusters"
   homepage "https://github.com/clowdhaus/eksup"
-  version "v0.12.0"
+  version "v0.13.0"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/clowdhaus/eksup/releases/download/#{version}/eksup-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "fcc294d237ed3419c98d01e94d22b9df923ed60d6d9644634233ba299c0221b7"
+    sha256 "3df0142d592fe85788bc337c46002aef903bcfb3f0de606b9c98bbfe64e401da"
   end
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/clowdhaus/eksup/releases/download/#{version}/eksup-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256  "c9712a11028d2c6c9ac954a0a424095caa58182fae7a29724c5a879c2b6ed4e2"
+    sha256  "19fed75e6d0988ec0701bc82539c3bfc99b327b8ec877a81d034b11706b6ff61"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/clowdhaus/eksup/releases/download/#{version}/eksup-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "7bb11f80868e522b0055dc46122e1e5f4d009b2088908beb59c99543e0eb7796"
+    sha256 "db1ea67a421c3b9ff0370deb7c1cbc5a621259d988a080b95f91b82b8cfeec1f"
   end
 
   if OS.linux? && Hardware::CPU.intel?
       url "https://github.com/clowdhaus/eksup/releases/download/#{version}/eksup-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1e17e638ba5de88d1312d66a08764b2691e7c2ad055f5c16f86b766681c8af0f"
+      sha256 "2522919bc451b3872562fcc91518ac807752aa7e9879b5335a96695d3e920138"
   end
 
   def install
